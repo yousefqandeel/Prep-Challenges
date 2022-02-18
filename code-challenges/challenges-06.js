b071f09562c75f2a9939e803a1da88a2e1e4d6e6
Yousef Qandeel <yqandil7@gmail.com>
2022-02-18 15:27:08 +0200

    fix a function

--- code-challenges/challenges-06.js
+++ code-challenges/challenges-06.js
@@ -34,8 +34,10 @@ const findMax = (arr) => {
 
 const sumNums = (arr) => {
     let sum = 0;
-    for (let i = 0; i < arr.length; i++)
+    for (let i = 0; i < arr.length; i++) {
         sum += arr[i];
+    }
+
     return sum;
 };
 // -------------------------------------------------------------------------------------------------------
