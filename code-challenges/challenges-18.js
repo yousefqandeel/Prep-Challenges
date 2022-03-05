e647f6beb66418b1f924ef538e5dd115106abcb0
Yousef Qandeel <yqandil7@gmail.com>
2022-03-06 02:06:51 +0300

    challenge 18

---
+++ code-challenges/challenges-18.js
@@ -0,0 +1,79 @@
+'use strict';
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 01:
+// Required:
+//
+// Given a string find the length of the middle word in the sentence
+//
+// Ex:
+// Input: "You can't handle the truth!"
+// Output: 6
+//
+// Input: "You're gonna need a bigger boat"
+// Output: 1
+//
+
+const wordLength = (str) => {
+    let arr = str.split(" ");
+    let middle = Math.floor(arr.length / 2);
+    return arr[middle].length;
+}
+// -------------------------------------------------------------------------------------------------------
+
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 02:
+// Required:
+//
+// Write a function that takes two strings and checks if the two strings contain the same combination of letters
+//
+// Ex:
+// Input: "dad", "add"
+// Output: true
+//
+// Input: "dad", "aadd"
+// Output: false
+//
+// Input: "table", "label"
+// Output: false
+//
+// Input: "cat", "rat"
+// Output: false
+
+const wordLetters = (str1, str2) => {
+    let arr1 = str1.split("").sort();
+    let arr2 = str2.split("").sort();
+    if (JSON.stringify(arr1) == JSON.stringify(arr2)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 03:
+// Optional:
+//
+// Write a function that takes an integer and a sorted array
+// The function returns the index of the integer in the array
+// If the integer was not there, the function returns where it could have been
+//
+// Ex:
+// Input: [1,3,5,6], 5
+// Output: 2
+//
+// Input: [1,3,5,6], 2
+// Output: 1
+//
+// Input: [1,3,5,6], 7
+// Output: 4
+//
+
+const targetIndex = (arr, int) => {
+    // write your code here
+}
+// -------------------------------------------------------------------------------------------------------
+
+module.exports = { wordLength, wordLetters, targetIndex };
